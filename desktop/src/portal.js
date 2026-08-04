5293dc8308cb950d1f849d38c4cb63532be83eed
suzywang168-oss <suzywang168@gmail.com>
2026-08-04 13:59:25 +0800

    Build Octopus desktop installers
    * Add desktop/package.json
    
    * Add desktop/src/app.js
    
    * Add desktop/src/portal.js
    
    * Add desktop/src/admin.js
    
    * Add desktop/electron-builder.portal.yml
    
    * Add desktop/electron-builder.admin.yml
    
    * Add desktop/README.md
    
    * Add .github/workflows/desktop-release.yml

---
+++ desktop/src/portal.js
@@ -0,0 +1,6 @@
+const { launchDesktopApp } = require("./app");
+
+launchDesktopApp({
+  title: "Octopus Release Portal",
+  url: "https://suzywang168-oss.github.io/octopus-release-admin/user-portal/v78/#/overview",
+});
