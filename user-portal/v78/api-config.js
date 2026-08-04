38dab6ee8cfb9e00759b8812cfe0fe9ebed8d4b8
suzywang168-oss <suzywang168@gmail.com>
2026-08-04 13:16:10 +0800

    Connect User Portal to Octopus shared API

---
+++ user-portal/v78/api-config.js
@@ -0,0 +1,7 @@
+(function () {
+  window.OCTOPUS_API_CONFIG = Object.freeze({
+    baseUrl: "https://octopus-admin-console.suzywang168.chatgpt.site",
+    sharedRecordsPath: "/api/shared/records",
+    version: 1
+  });
+})();
